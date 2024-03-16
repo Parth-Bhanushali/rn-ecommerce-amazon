3728386e9b4c5fb64f2f62af68396ab6d82e8ba3
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2024-03-16 11:42:41 +0530

    Design Home screen - part 1

---
+++ src/components/ProductItem.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native'
+import React from 'react'
+
+export default function ProductItem({item}) {
+  return (
+    <Pressable>
+      <Image 
+        resizeMode='contain'
+        source={{ uri: item?.image }}
+        style={{ width: 150, height: 150}}
+      />
+
+      <Text numberOfLines={1} style={{ width: 150, marginTop: 10 }}>{item?.title}</Text>
+    
+      <View style={{ marginTop: 5, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', }}>
+        <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{item?.price}</Text>
+        <Text style={{ color: 'gray', fontWeight: 'bold' }}>{item?.rating?.rate} ratings</Text>
+      </View>
+
+      <Pressable style={{ backgroundColor: '#FFC72C', padding: 10, borderRadius: 20, justifyContent: 'center', alignItems: 'center', marginHorizontal: 10, marginTop: 10 }}>
+        <Text>Add to Cart</Text>
+      </Pressable>
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({})
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,137 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, Platform, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import { AntDesign, Feather, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { SliderBox } from 'react-native-image-slider-box';
+import axios from 'axios'
+import ProductItem from '../components/ProductItem'
+import { categories, carouselImages, deals, offers, fakeProductsUrl } from '../data/dummy'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState()
+
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+          const response = await axios.get(fakeProductsUrl)
+          setProducts(response.data)
+      } catch (error) {
+          console.log("Failed to fetch products data: ", error);
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-      <Text>HomeScreen</Text>
+    <SafeAreaView 
+      style={{ 
+        flex: 1, backgroundColor: 'white',
+      }}
+    >
+      <ScrollView>
+        <View style={{ backgroundColor: '#00CED1', padding: 10, flexDirection: 'row', alignItems: 'center',}}>
+          <Pressable style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: 8, gap: 10, backgroundColor: 'white', borderRadius: 3, height: 40, flex: 1}}>
+            <AntDesign name='search1' size={20} color={'black'} style={{ paddingLeft: 10 }} />
+            <TextInput 
+              placeholder='Search amazon.in'
+            />
+          </Pressable>
+
+          <Pressable style={{ flexDirection: 'row', alignItems: 'center', height: 40}}>
+            <Feather name='mic' size={22} color={'black'} style={{ paddingHorizontal: 10 }} />
+          </Pressable>
+        </View>
+
+        <Pressable style={{ flexDirection: 'row', alignItems: 'center', gap: 8, padding: 10, backgroundColor: '#AFEEEE', }}>
+          <Ionicons name='location-outline' size={20} color={'black'} />
+
+          <View>
+            <Text style={{ fontSize: 13, fontWeight: '500' }}>Deliver to Parth - Pune 411011</Text>
+          </View>
+
+          <MaterialIcons name='keyboard-arrow-down' size={24} color={'black'} />
+        </Pressable>
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ gap: 16, paddingHorizontal: 12 }}>
+          {
+            categories.map((item, index) => (
+              <Pressable key={item.id} style={{ marginVertical: 10, justifyContent: 'center', alignItems: 'center' }}>
+                <Image
+                  source={{ uri: item.image}}
+                  style={{ width:50, height: 50 }}
+                  resizeMode='contain'
+                />
+
+                <Text style={{ textAlign: 'center', fontSize: 12, fontWeight: '500', marginTop: 5, marginBottom: 3 }}>{item?.name}</Text>
+              </Pressable>
+            ))
+          }
+        </ScrollView>
+
+        <SliderBox
+          images={carouselImages}
+          autoplay
+          circleLoop
+          dotColor={'#13274F'}
+          inactiveDotColor={'#90A4AE'}
+          ImageComponentStyle={{ width: '100%' }}
+        />
+
+        <Text style={{ paddingHorizontal: 10, paddingVertical: 16, fontSize: 18, fontWeight: 'bold' }}>Trending deals of the week</Text>
+      
+        <View style={{ flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-around', width: '100%' }}>
+          {
+            deals.map((item, index) => (
+              <Pressable style={{ marginVertical: 10 }}>
+                <Image 
+                  source={{ uri: item?.image }}
+                  style={{ width: 200, height: 200 }}
+                  resizeMode='contain'
+                />
+              </Pressable>
+            ))
+          }
+        </View>
+
+        <View style={{ borderWidth: 1, borderColor: '#D0D0D0', height: 1, marginTop: 16 }} />
+
+        <Text style={{ paddingHorizontal: 10, paddingVertical: 16, fontSize: 18, fontWeight: 'bold' }}>Today's deals</Text>
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {
+            offers.map((item, index) => (
+              <Pressable key={index} style={{ marginTop: 16, marginBottom: 16, alignItems: 'center', justifyContent: 'center' }}>
+                <Image
+                  source={{ uri: item?.image }}
+                  style={{ width: 150, height: 150 }}
+                  resizeMode='contain'
+                />
+
+                <View 
+                  style={{ 
+                    backgroundColor: 'rgb(120,121,250)', justifyContent: 'center', alignItems: 'center', width: 130, paddingVertical: 5,
+                    borderRadius: 4, marginTop: 16
+                  }}
+                >
+                  <Text style={{ fontSize: 13, fontWeight: 'bold', color: 'white' }}>Upto {item?.offer} off</Text>
+                </View>
+              </Pressable>
+            ))
+          }
+        </ScrollView>
+
+        <View style={{ borderWidth: 1, borderColor: '#D0D0D0', height: 1, marginVertical: 8 }} />
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap'}}>
+          {
+            products?.map((item, index) => (
+              <View key={index} style={{ marginVertical: 12 }}>
+                <ProductItem item={item} />
+              </View>
+            ))
+          }
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
